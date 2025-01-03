7df419974678f34a3f3e6c5010dc3518f50a9b15
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-01-04 00:23:23 +0100

    Renamed the file

--- src/test/java/com/chatroom/ChatTests.java
+++ src/test/java/com/chatroom/ChatTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ChatTest {
+public class ChatTests {
     private TextMessageService textMessageService;
 
     @BeforeEach
